0a775ad7cefc9ace01379de8c7d0d265c101bdf8
PauloFernando1994 <55366717+PauloFernando1994@users.noreply.github.com>
2020-09-09 17:38:19 -0300

    update

---
+++ src/MercadoLivre/Api/Auth/Auth.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Api;
+
+
+class Auth
+{
+
+
+    public function __construct()
+    {
+
+    }
+}
---
+++ src/MercadoLivre/Facades/Support.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Facades;
+
+
+trait Support
+{
+
+}
